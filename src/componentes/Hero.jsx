2bd163087b6ac5ebf8ea643fc74ef70afe9f732a
lumens7 <142345222+lumens7@users.noreply.github.com>
2026-06-07 11:18:20 -0300

    ajuste quantidade santos

--- src/componentes/Hero.jsx
+++ src/componentes/Hero.jsx
@@ -12,7 +12,7 @@ export default function Hero() {
 
     const buscarSanto = async () => {
 
-      const MAX_ID = 50; // ajuste conforme total do seu banco
+      const MAX_ID = 60; // ajuste conforme total do seu banco
       const idsTestados = new Set();
 
       try {
