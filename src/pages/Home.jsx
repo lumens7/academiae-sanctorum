200602f90575025c199e32fe863aeda4c0df3929
lumens7 <142345222+lumens7@users.noreply.github.com>
2026-06-17 22:09:10 -0300

    update home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -54,7 +54,7 @@ export default function Home() {
       // const MAX_ID = 10;
       // const QUANTIDADE = 9;
 
-      const MAX_ID = 87;
+      const MAX_ID = 113;
       const QUANTIDADE = 9;
 
       const santosEncontrados = [];
@@ -112,7 +112,7 @@ export default function Home() {
 
       // const MAX_ID = 12;
       // const QUANTIDADE = 9;
-      const MAX_ID = 87;
+      const MAX_ID = 113;
       const QUANTIDADE = 9;
 
       const lista = [];
@@ -191,7 +191,7 @@ export default function Home() {
         }
 
         // fallback: busca qualquer novena existente
-        for (let id = 1; id <= 60 ; id++) {
+        for (let id = 1; id <= 113 ; id++) {
 
           res = await fetch(`${API_URL}/api/santo/${id}/novena`);
 
