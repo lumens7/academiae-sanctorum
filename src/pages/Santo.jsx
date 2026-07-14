4add09865d51623ef8ee995319053025c2648b29
lumens7 <142345222+lumens7@users.noreply.github.com>
2026-07-13 21:14:28 -0300

    update santo

--- src/pages/Santo.jsx
+++ src/pages/Santo.jsx
@@ -114,7 +114,7 @@ export default function Santo() {
         // const MAX_ID = 20;
         // const QUANTIDADE = 3;
 
-        const MAX_ID = 113;
+        const MAX_ID = 203;
         const QUANTIDADE = 9;
 
         const santosEncontrados = [];
